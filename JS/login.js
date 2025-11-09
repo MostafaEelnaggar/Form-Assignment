8d7c58e9e995f33929ab0bc60831d26fcaddfe01
Mostafa El Naggar <elnaggarmostafa421@gmail.com>
2025-11-09 18:43:16 +0200

    edit

--- JS/login.js
+++ JS/login.js
@@ -1,28 +1,41 @@
 //==================== Start Log In JS Section ======================
 var loginBut = document.querySelector(".loginBut");
-  var loginEmail = document.querySelector("#userLoginEmail");
-  var loginPass = document.querySelector("#userLoginPass");
-  var e1 = document.querySelector(".e1");
-  var e2 = document.querySelector(".e2");
-  var e3 = document.querySelector(".e3");
+var loginEmail = document.querySelector("#userLoginEmail");
+var loginPass = document.querySelector("#userLoginPass");
+var e1 = document.querySelector(".e1");
+var e2 = document.querySelector(".e2");
+var e3 = document.querySelector(".e3");
 
-  loginBut.addEventListener("click", function (e) {
+loginBut.addEventListener("click", function (e) {
     e.preventDefault();
     const user = JSON.parse(localStorage.getItem('user'));
-    if (
-      user.email === loginEmail.value.trim() &&
-      user.password === loginPass.value.trim()
-    ) {
-      window.location.href = "../html/welcom.html";
-    } else if (localStorage.getItem("email") !== loginEmail.value.trim()) {
-      e1.classList.remove("d-none");
-      e3.classList.remove("d-none");
-    } else if (localStorage.getItem("password") !== loginPass.value.trim()) {
-      e2.classList.remove("d-none");
-      e3.classList.remove("d-none");
+
+    if (!user) {
+        window.alert("Please sign up first");
+        e3.classList.remove("d-none");
     } else {
-         window.alert("Please enter valid inputs");
-         e3.classList.remove("d-none");
+        if (
+            user.email === loginEmail.value.trim() &&
+            user.password === loginPass.value.trim()
+        ) {
+            window.location.href = "../html/welcom.html";
+        }
+        else if (loginEmail.value.trim() === "" && loginPass.value.trim() === "") {
+            e1.classList.remove("d-none");
+            e2.classList.remove("d-none");
+            e3.classList.remove("d-none");
+        }
+        else if (user.email !== loginEmail.value.trim()) {
+            e1.classList.remove("d-none");
+            e3.classList.remove("d-none");
+        } else if (user.password !== loginPass.value.trim()) {
+            e2.classList.remove("d-none");
+            e3.classList.remove("d-none");
+        }
+        else {
+            window.alert("Please enter valid inputs");
+            e3.classList.remove("d-none");
+        }
     }
-  });
+});
 // ============================= End Log In JS Section =======================
